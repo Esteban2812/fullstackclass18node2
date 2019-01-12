c4f2e6c842b6b853c1e028dfaf81f2dcaf8c98fd
Alonzo <educatech1gmail.com>
2019-01-11 21:39:03 -0500

    Pipes

--- Angular/Clase09/appPipes/src/app/app.module.ts
+++ Angular/Clase09/appPipes/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReducidorPipe } from './reducidor.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReducidorPipe
   ],
   imports: [
     BrowserModule
